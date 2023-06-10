552cb457793cb319c80dd739d2559e10e9a13679
Jaber <njb99m@gmail.com>
2023-06-10 13:00:50 +0600

    Laravel Project

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json($tasks);
+    }
+
+    public function store(Request $request)
+    {
+        $task = Task::create($request->all());
+        return response()->json($task, 201);
+    }
+
+    public function show($id)
+    {
+        $task = Task::findOrFail($id);
+        return response()->json($task);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+        return response()->json($task);
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return response()->json(null, 204);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('demo/home');
+});
+
+Route::get('/about', function () {
+    return view('demo/about');
+});
+
+Route::get('/contact', function () {
+    return view('demo/contact');
+});
+
+Route::get('/product', function () {
+    return view('demo/product');
+});
+
+Route::get('/service', function () {
+    return view('demo/service');
+});
+
+Route::get('/login', function () {
+    return view('demo/login');
+});
+
+Route::get('/register', function () {
+    return view('demo/register');
+});
+
+Route::get('/category', function () {
+    return view('demo/category');
+});
+
+Route::get('/cart', function () {
+    return view('demo/cart');
+});
+
+Route::get('/offer', function () {
+    return view('demo/offer');
+});
