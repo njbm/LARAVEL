72eb55509867643e986dce798e983792c9455646
Jaber Masud <njb99m@gmail.com>
2023-10-01 13:19:59 +0600

    single user add

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Validator;
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -20,7 +20,27 @@ class UserApiController extends Controller
     public function addUser(Request $request){
         if($request->ismethod('post')){
             $data = $request->all();
-            // return $data;
+
+            $rules=[
+                'name'=>'required',
+                'email'=>'required|email|unique:users',
+                'password'=>'required',
+            ];  
+
+            $flushMessage=[
+                'name.required'=>'Name field is Required',
+                'email.required'=>'Email field is Required',
+                'email.email'=>'Email must be a Valid email',
+                'email.unique' => 'Email is already in use',
+                'password.required'=>'Password field is Required',
+            ];
+
+            $validate =Validator::make($data, $rules, $flushMessage);
+
+            if($validate->fails()){
+                return response()->json($validate->errors(), 422);
+            }
+
             $user = new User();
             $user->name = $data['name'];
             $user->email = $data['email'];
@@ -30,4 +50,10 @@ class UserApiController extends Controller
             return response()->json(['message'=>$message], 201);
         }
     }
+
+
+
+    // public function masud(){
+    //     return 'Hi I am Jaber Masud';
+    // }
 }
